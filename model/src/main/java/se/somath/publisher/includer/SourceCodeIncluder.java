98e369d34dac87eb172acde3c1390e4d9f55b0ed
tsu <tsu@kth.se>
2012-07-24 15:53:27 +0200

    Added handling include on multiple lines

--- model/src/main/java/se/somath/publisher/includer/SourceCodeIncluder.java
+++ model/src/main/java/se/somath/publisher/includer/SourceCodeIncluder.java
@@ -1,28 +1,65 @@
 package se.somath.publisher.includer;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class SourceCodeIncluder {
-
     private SourceCodeReader sourceCodeReader;
-    private IncludeParser includeParser;
+    private IncludeParser includeParser = new IncludeParser();
 
-    public List<String> getIncludes(List<String> content) {
+    public List<String> addIncludes(List<String> content) {
+
+        List<String> result = new LinkedList<String>();
+
+        String includeTag = "";
+        boolean startTagFound = false;
+        List<String> sourceCode = null;
 
         for (String candidate : content) {
-            if (candidate.startsWith("<include")) {
-                includeParser.parse(candidate);
+            if (candidate.startsWith("<include") && candidate.endsWith("/>")) {
+                includeTag = candidate;
+
+                includeParser.parse(includeTag);
+                includeTag = "";
+
+                String root = includeParser.getRoot();
+                String fileName = includeParser.getFileName();
+
+                sourceCodeReader.readFile(root, fileName);
+            }
+
+            if (candidate.startsWith("<include") && !candidate.endsWith("/>")) {
+                includeTag = candidate;
+                startTagFound = true;
+                continue;
+            }
+
+            if (!candidate.startsWith("<include") && candidate.endsWith("/>")) {
+                includeTag += candidate;
+
+                includeParser.parse(includeTag);
+                includeTag = "";
 
                 String root = includeParser.getRoot();
                 String fileName = includeParser.getFileName();
 
                 sourceCodeReader.readFile(root, fileName);
 
+                startTagFound = false;
+            }
+
+            if (startTagFound) {
+                includeTag += candidate;
+            } else {
+                result.add(candidate);
+            }
+
+            if (sourceCode != null) {
+                result.addAll(sourceCode);
             }
         }
 
-
-        return content;
+        return result;
     }
 
     public void setSourceCodeReader(SourceCodeReader sourceCodeReader) {
--- model/src/test/java/se/somath/publisher/includer/SourceCodeIncluderTest.java
+++ model/src/test/java/se/somath/publisher/includer/SourceCodeIncluderTest.java
@@ -11,22 +11,56 @@ import static org.mockito.Mockito.verify;
 public class SourceCodeIncluderTest {
 
     @Test
-    public void shouldIncludeAFile() {
+    public void shouldCallReadFileWithIncludeOnOneLine() {
         List<String> given = new LinkedList<String>();
 
         given.add("<include root=\"fileRoot\" file=\"fileRelativeTheRoot\"/>");
 
-        SourceCodeIncluder includer = new SourceCodeIncluder();
         SourceCodeReader sourceCodeReader = mock(SourceCodeReader.class);
-        includer.setSourceCodeReader(sourceCodeReader);
+        SourceCodeIncluder includer = getSourceCodeIncluder(sourceCodeReader);
 
-        IncludeParser includeParser = new IncludeParser();
-        includer.setIncludeParser(includeParser);
-
-        includer.getIncludes(given);
+        includer.addIncludes(given);
 
         verify(sourceCodeReader).readFile("fileRoot", "fileRelativeTheRoot");
     }
 
+    @Test
+    public void shouldCallReadFileWithIncludeOnTwoLines() {
+        List<String> given = new LinkedList<String>();
 
+        given.add("<include root=\"fileRoot\" ");
+        given.add("file=\"fileRelativeTheRoot\"/>");
+
+        SourceCodeReader sourceCodeReader = mock(SourceCodeReader.class);
+        SourceCodeIncluder includer = getSourceCodeIncluder(sourceCodeReader);
+
+        includer.addIncludes(given);
+
+        verify(sourceCodeReader).readFile("fileRoot", "fileRelativeTheRoot");
+    }
+
+    @Test
+    public void shouldCallReadFileWithIncludeOnThreeLines() {
+        List<String> given = new LinkedList<String>();
+
+        given.add("<include ");
+        given.add("root=\"fileRoot\" ");
+        given.add("file=\"fileRelativeTheRoot\"/>");
+
+        SourceCodeReader sourceCodeReader = mock(SourceCodeReader.class);
+        SourceCodeIncluder includer = getSourceCodeIncluder(sourceCodeReader);
+
+        includer.addIncludes(given);
+
+        verify(sourceCodeReader).readFile("fileRoot", "fileRelativeTheRoot");
+    }
+
+    private SourceCodeIncluder getSourceCodeIncluder(SourceCodeReader sourceCodeReader) {
+        SourceCodeIncluder includer = new SourceCodeIncluder();
+        includer.setSourceCodeReader(sourceCodeReader);
+
+        IncludeParser includeParser = new IncludeParser();
+        includer.setIncludeParser(includeParser);
+        return includer;
+    }
 }
