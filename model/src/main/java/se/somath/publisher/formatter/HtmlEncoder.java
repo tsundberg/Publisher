b46d48ccd05bd40c487dadb35e9a743a25dd2c99
tsu <tsu@kth.se>
2012-07-25 14:52:29 +0200

    Added implemented transforming and updated acceptance criteria

--- model/src/main/java/se/somath/publisher/formatter/HtmlEncoder.java
+++ model/src/main/java/se/somath/publisher/formatter/HtmlEncoder.java
@@ -1,9 +1,22 @@
 package se.somath.publisher.formatter;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class HtmlEncoder {
     public List<String> encode(List<String> sourceCode) {
-        return sourceCode;
+        List<String> result = new LinkedList<String>();
+
+        for (String tmp : sourceCode) {
+            tmp = tmp.replaceAll("&", "&amp;");
+            tmp = tmp.replaceAll("<", "&lt;");
+            tmp = tmp.replaceAll(">", "&gt;");
+            tmp = tmp.replaceAll("'", "&apos;");
+            tmp = tmp.replaceAll("\"", "&quot;");
+
+            result.add(tmp);
+        }
+
+        return result;
     }
 }
--- model/src/test/java/se/somath/publisher/formatter/HtmlEncoderTest.java
+++ model/src/test/java/se/somath/publisher/formatter/HtmlEncoderTest.java
@@ -1,6 +1,38 @@
 package se.somath.publisher.formatter;
 
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class HtmlEncoderTest {
+
+    @Test
+    public void shouldHtmlEncode() {
+        HtmlEncoder encoder = new HtmlEncoder();
+        List<String> given = new LinkedList<String>();
+        given.add("&");
+        given.add("<");
+        given.add(">");
+        given.add("'");
+        given.add("\"");
+
+        List<String> expected = new LinkedList<String>();
+        expected.add("&amp;");
+        expected.add("&lt;");
+        expected.add("&gt;");
+        expected.add("&apos;");
+        expected.add("&quot;");
+
+        List<String> actual = encoder.encode(given);
+
+        assertThat(actual, is(expected));
+    }
+
+
     // todo add proper test
     // Går det att skapa en funktion som plockar alla filer och bygger en grafisk representation av dom så man enkelt kan se vilken fil som hör hemma i vilken katalog?
     // Ta en
