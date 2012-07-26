597fe70cfcc0393b138f97a7790cb07605ea7558
tsu <tsu@kth.se>
2012-07-26 09:48:20 +0200

    House keeping

--- model/src/test/java/se/somath/publisher/formatter/HtmlEncoderTest.java
+++ model/src/test/java/se/somath/publisher/formatter/HtmlEncoderTest.java
@@ -31,12 +31,4 @@ public class HtmlEncoderTest {
 
         assertThat(actual, is(expected));
     }
-
-
-    // todo add proper test
-    // Går det att skapa en funktion som plockar alla filer och bygger en grafisk representation av dom så man enkelt kan se vilken fil som hör hemma i vilken katalog?
-    // Ta en
-    // * root
-    // * en exclude
-    // skapa ascii grafik för resten av filerna
 }
